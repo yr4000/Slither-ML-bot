4634bb14e23a7e2c8e7420a318ff01f56dcb2967
Yair <y4000r@gmail.com>
2017-08-10 16:11:12 +0300

    added utils folder with skeletons

---
+++ utils/plot_utils.py
@@ -0,0 +1,14 @@
+'''
+This file will contain all the functions which we will use in order to plot graphic
+representations of our experiments results.
+'''
+
+import matplotlib
+matplotlib.use('Qt4Agg')    #TODO: not sure it is necessary...
+#matplotlib.use('Agg') #in order not to display the plot
+import matplotlib.pyplot as plt
+
+def plot_graph(points):
+    plt.plot(points)
+    plt.title("title")  #TODO: the title should not be the same all the time but dynamic according to some parameters
+    plt.savefig("..\\..\\graphs\\file_name")    #TODO: so do the file name
---
+++ utils/process_utils.py
@@ -0,0 +1,25 @@
+'''
+This file will contain all the functions that process the incformation
+in order to turn it to a vector which will be the input of the model
+'''
+
+'''
+Input: a snakes object (dict)
+Output: a vector which contains significant data about the snakes
+'''
+def process_snakes(snakes):
+    pass
+
+'''
+Input: a foods object (dict)
+Output: a vector which contains significant data about the foods
+'''
+def process_foods(foods):
+    pass
+
+'''
+Input: a snake object (it's not the same as snakes object!)
+Output: a vector which contains significant data about the agents snake
+'''
+def process_my_snake(snake):
+    pass
