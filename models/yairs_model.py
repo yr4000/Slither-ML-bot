53ceefb47341419fb85c1a87162b9eb3eda163f5
Yair <y4000r@gmail.com>
2017-08-13 18:53:58 +0300

    attempt to create model

--- models/yairs_model.py
+++ models/yairs_model.py
@@ -8,3 +8,58 @@ with open('../observation.json') as json_data:
     d = json.load(json_data)
     print(d)
 
+
+OUTPUT_DIM = 8
+INPUT_DIM = 400
+SQRT_INPUT_DIM  =20 #IN ORDER TO RESHAPE INTO TENSOR
+CONV_WINDOW_SIZE = 5
+NUM_OF_CHANNELS_LAYER1 = 1
+NUM_OF_CHANNELS_LAYER2 = 16
+NUM_OF_CHANNELS_LAYER3 = 32
+SIZE_OF_FULLY_CONNECTED_LAYER = 256
+
+keep_rate = 0.8
+keep_prob = tf.placeholder(tf.float32)
+
+# build the computational graph
+states = tf.placeholder(tf.int8, [None, INPUT_DIM], name='states')
+actions = tf.placeholder(tf.int8, [None, OUTPUT_DIM], name='chosen_actions')
+accu_rewards = tf.placeholder(tf.float32, [None, OUTPUT_DIM], name='accumulated_rewards')
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def maxpool2d(x):
+    #                        size of window         movement of window
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def convolutional_neural_network(x):
+    weights = {'W_conv1': tf.Variable(tf.random_normal([CONV_WINDOW_SIZE , CONV_WINDOW_SIZE , NUM_OF_CHANNELS_LAYER1 , NUM_OF_CHANNELS_LAYER2])),
+               'W_conv2': tf.Variable(tf.random_normal([CONV_WINDOW_SIZE , CONV_WINDOW_SIZE , NUM_OF_CHANNELS_LAYER2, NUM_OF_CHANNELS_LAYER3])),
+               'W_fc': tf.Variable(tf.random_normal([CONV_WINDOW_SIZE  * CONV_WINDOW_SIZE  * NUM_OF_CHANNELS_LAYER3, SIZE_OF_FULLY_CONNECTED_LAYER])),
+               'out': tf.Variable(tf.random_normal([SIZE_OF_FULLY_CONNECTED_LAYER, OUTPUT_DIM]))}
+
+    biases = {'b_conv1': tf.Variable(tf.random_normal([NUM_OF_CHANNELS_LAYER2])),
+              'b_conv2': tf.Variable(tf.random_normal([NUM_OF_CHANNELS_LAYER3])),
+              'b_fc': tf.Variable(tf.random_normal([SIZE_OF_FULLY_CONNECTED_LAYER])),
+              'out': tf.Variable(tf.random_normal([OUTPUT_DIM]))}
+
+    x = tf.reshape(x, shape=[-1, SQRT_INPUT_DIM, SQRT_INPUT_DIM, NUM_OF_CHANNELS_LAYER1])
+
+    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
+    conv1 = maxpool2d(conv1)
+
+    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
+    conv2 = maxpool2d(conv2)
+
+    fc = tf.reshape(conv2, [-1, CONV_WINDOW_SIZE * CONV_WINDOW_SIZE * NUM_OF_CHANNELS_LAYER3])
+    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
+    fc = tf.nn.dropout(fc, keep_rate)
+
+    score = tf.matmul(fc, weights['out']) + biases['out']
+    probability = tf.nn.softmax(score)
+    return probability
+
+
