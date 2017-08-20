2b7b3378e9d17d085096492532382b05fb43cd7a
Yair <y4000r@gmail.com>
2017-08-21 02:11:22 +0300

    small changes in plot_utils

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -8,7 +8,8 @@ matplotlib.use('Qt4Agg')    #TODO: not sure it is necessary...
 #matplotlib.use('Agg') #in order not to display the plot
 import matplotlib.pyplot as plt
 
-def plot_graph(points):
+def plot_graph(points,title,file_name):
     plt.plot(points)
-    plt.title("title")  #TODO: the title should not be the same all the time but dynamic according to some parameters
-    plt.savefig("..\\..\\graphs\\file_name")    #TODO: so do the file name
+    plt.title(title)  #TODO: the title should not be the same all the time but dynamic according to some parameters
+    #plt.savefig("..\\..\\graphs\\file_name")    #TODO: so do the file name
+    plt.savefig(file_name)
