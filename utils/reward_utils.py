bd57e23cc0d09e76961060e2a542bd98d667c295
Tom Garburg <tomgarburg@gmail.com>
2017-08-22 10:07:37 +0300

    chnged the reward utils to fit Yair's changes

--- utils/reward_utils.py
+++ utils/reward_utils.py
@@ -1,11 +1,15 @@
 import numpy as np
 #hyper parameters for reward function
-DISCOUNT_FACTOR = 0.95
-BACKWARDS_RELEVANCY_WINDOW = 10
-BOOSTING_FACTOR = 3.0
+DISCOUNT_FACTOR = 0.99
+#BACKWARDS_RELEVANCY_WINDOW = 10
+#BOOSTING_FACTOR = 3.0
 PUNISHMENT_FOR_DEATH = -100
-NO_REWARD_PENALTY = -0.05
+NO_REWARD_PENALTY = -1
+SMALL_GAIN_REWARD = 2
+MEDIUM_GAIN_REWARD = 5
+LARGE_GAIN_REWARD = 10
 
+'''
 #boosts by BOOSTING_FACTOR all the BACKWARDS_RELEVANCY_WINDOW rewards before an
 #actual point was earned in the game , and rewards at step when accelaration took place
 def boost(reward_indicator, accel_penalty_indicator, processed_rewards):
@@ -18,32 +22,42 @@ def boost(reward_indicator, accel_penalty_indicator, processed_rewards):
     boosting_indicator = (np.logical_or( reward_indicator , accel_penalty_indicator)).astype(np.int)
     boosting_indicator = np.add(np.multiply(boosting_indicator, BOOSTING_FACTOR - 1), 1)
     return np.multiply(boosting_indicator , processed_rewards)
-
+'''
 #calculates rewards per step from the score per step array
 #TODO: need to take care when the array is at size 2
 def calc_reward_from_raw(score_arr , is_dead):
     if (len(score_arr) == 1):
         return np.array([0]) # worst case TODO : i think should never happen im model
+    rewards = []
+    scores_diff = np.diff(score_arr)    #convert raw score to points earned/lost per step
+    for k in scores_diff:
+        if(k<=0):
+            rewards.append(NO_REWARD_PENALTY)
+        elif(k>0 and k<=5):
+            rewards.append(SMALL_GAIN_REWARD)
+        elif(k>5 and k<=15):
+            rewards.append(MEDIUM_GAIN_REWARD)
+        else:
+            rewards.append(LARGE_GAIN_REWARD)
 
-    rewards = np.diff(score_arr)    #convert raw score to points earned/lost per step
+
     if (is_dead):
         rewards[len(rewards)-1] = PUNISHMENT_FOR_DEATH      #TODO: I am not sure this is true, we shouldn't update only in death
 
-    #"penalize" steps with reward == 0 with negative reward
-    rewards = np.add(rewards, np.multiply((rewards == 0).astype(int), NO_REWARD_PENALTY))
     #compute discounted rewards
     discounted_formula = np.frompyfunc(lambda x, y: DISCOUNT_FACTOR * x + y, 2, 1)
     cumulative_discounted_rewards= np.flip(
                 discounted_formula.accumulate(np.flip(rewards, 0), dtype=np.object).astype(np.double),0)
 
     #boost relevant rewards
-    boosted_processed_rewards = boost( (rewards > 0) ,(rewards < NO_REWARD_PENALTY)
-                                       , cumulative_discounted_rewards)
+    #boosted_processed_rewards = boost( (rewards > 0) ,(rewards < NO_REWARD_PENALTY)
+    #
+    #                                    , cumulative_discounted_rewards)
     #normalize
-    boosted_processed_rewards = np.divide(boosted_processed_rewards, np.std(boosted_processed_rewards))
-    boosted_processed_rewards -= np.mean(boosted_processed_rewards)
+    normalized_rewards = np.divide(boosted_processed_rewards, np.std(boosted_processed_rewards))
+    normalized_rewards -= np.mean(boosted_processed_rewards)
 
-    return boosted_processed_rewards
+    return normalized_rewards
 
 def main():
     a = np.array(range(60))
--- utils/toms_model.py
+++ utils/toms_model.py
@@ -1,55 +1,252 @@
-import numpy as np
-#hyper parameters for reward function
-DISCOUNT_FACTOR = 0.95
-BACKWARDS_RELEVANCY_WINDOW = 10
-BOOSTING_FACTOR = 3.0
-PUNISHMENT_FOR_DEATH = -50
-NO_REWARD_PENALTY = -0.05
+from utils.models_utils import *
+from utils.reward_utils import calc_reward_from_raw
+import pickle as pkl
+import os
+import time
 
-#boosts by BOOSTING_FACTOR all the BACKWARDS_RELEVANCY_WINDOW rewards before an
-#actual point was earned in the game , and rewards at step when accelaration took place
-def boost(reward_indicator, accel_penalty_indicator, processed_rewards):
-    for i in (range(len(reward_indicator))):
-        if(reward_indicator[i]):#there was a reward in the i'th step
-            for j in (range(min(i , BACKWARDS_RELEVANCY_WINDOW))):#mark up to BACKWARDS_RELEVANCY_WINDOW
-                                                                  #previous actions as relevant
-                reward_indicator[i - j] = True
+#TODO: I am not sure if there is a problem here with the Qt thing or not
+import matplotlib
+matplotlib.use('Qt4Agg')
 
-    boosting_indicator = (np.logical_or( reward_indicator , accel_penalty_indicator)).astype(np.int)
-    boosting_indicator = np.add(np.multiply(boosting_indicator, BOOSTING_FACTOR - 1), 1)
-    return np.multiply(boosting_indicator , processed_rewards)
+#CNN constants
+OUTPUT_DIM = 64
+INPUT_DIM = 400
+SQRT_INPUT_DIM  = 20 #IN ORDER TO RESHAPE INTO TENSOR
+PLN = 2                     #Pool Layers Number
+CONV_WINDOW_SIZE = int(SQRT_INPUT_DIM / 2**PLN)
+NUM_OF_CHANNELS_LAYER1 = 1
+NUM_OF_CHANNELS_LAYER2 = 16     #TODO: Is that really what suppose to be here?
+NUM_OF_CHANNELS_LAYER3 = 32
+SIZE_OF_FULLY_CONNECTED_LAYER = 256
+VAR_NO = 8      #number of Ws and bs (the variables)
+KEEP_RATE = 0.8
+keep_prob = tf.placeholder(tf.float32)      #TODO: do we use that?
 
-#calculates rewards per step from the score per step array
-def calc_reward_from_raw(score_arr , is_dead):
-    if (len(score_arr) == 1):
-        return np.array([0]) # worst case TODO : i think should never happen im model
+#Model constants
+MAX_GAMES = 10000
+STEPS_UNTIL_BACKPROP = 50
+BATCH_SIZE = 50
 
-    rewards = np.diff(score_arr)    #convert raw score to points earned/lost per step
-    if (is_dead):
-        rewards[len(rewards)-1] = PUNISHMENT_FOR_DEATH      #TODO: I am not sure this is true, we shouldn't update only in death
+#Load and save constants
+WEIGHTS_FILE = 'weights.pkl'
+BEST_WEIGHTS = 'best_weights.pkl'
+LOAD_WEIGHTS = False
 
-    #"penalize" steps with reward == 0 with negative reward
-    rewards = np.add(rewards, np.multiply((rewards == 0).astype(int), NO_REWARD_PENALTY))
-    #compute discounted rewards
-    discounted_formula = np.frompyfunc(lambda x, y: DISCOUNT_FACTOR * x + y, 2, 1)
-    cumulative_discounted_rewards= np.flip(
-                discounted_formula.accumulate(np.flip(rewards, 0), dtype=np.object).astype(np.double),0)
+#other constants:
+BEGINING_SCORE = 10
 
-    #boost relevant rewards
-    boosted_processed_rewards = boost( (rewards > 0) ,(rewards < NO_REWARD_PENALTY)
-                                       , cumulative_discounted_rewards)
-    #normalize
-    boosted_processed_rewards = np.divide(boosted_processed_rewards, np.std(boosted_processed_rewards))
-    boosted_processed_rewards -= np.mean(boosted_processed_rewards)
 
-    return boosted_processed_rewards
 
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def maxpool2d(x):
+    #                        size of window         movement of window
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+def InitializeVarXavier(var_name,var_shape):
+    return tf.get_variable(name=var_name, shape=var_shape, dtype= tf.float32,
+                    initializer=tf.contrib.layers.xavier_initializer())
+
+initialize = InitializeVarXavier
+
+
+observations = tf.placeholder(tf.float32, [None,INPUT_DIM])
+#trainable variables
+weights = {'W_conv1': initialize('wc1',[CONV_WINDOW_SIZE , CONV_WINDOW_SIZE , NUM_OF_CHANNELS_LAYER1 , NUM_OF_CHANNELS_LAYER2]),
+           'W_conv2':  initialize('wc2',[CONV_WINDOW_SIZE , CONV_WINDOW_SIZE , NUM_OF_CHANNELS_LAYER2, NUM_OF_CHANNELS_LAYER3]),
+           'W_fc': initialize('wfc',[CONV_WINDOW_SIZE  * CONV_WINDOW_SIZE  * NUM_OF_CHANNELS_LAYER3, SIZE_OF_FULLY_CONNECTED_LAYER]),
+           'out': initialize('wo',[SIZE_OF_FULLY_CONNECTED_LAYER, OUTPUT_DIM])}
+
+
+biases = {'b_conv1': initialize('bc1', [NUM_OF_CHANNELS_LAYER2]),
+          'b_conv2': initialize('bc2', [NUM_OF_CHANNELS_LAYER3]),
+          'b_fc': initialize('bfc', [SIZE_OF_FULLY_CONNECTED_LAYER]),
+          'out': initialize('bo', [OUTPUT_DIM])}
+
+#CNN:
+x = tf.reshape(observations, shape=[-1, SQRT_INPUT_DIM, SQRT_INPUT_DIM, NUM_OF_CHANNELS_LAYER1])
+#first layer: conv + pool
+conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
+pool1 = maxpool2d(conv1)
+#second layer: conv + pool
+conv2 = tf.nn.relu(conv2d(pool1, weights['W_conv2']) + biases['b_conv2'])
+pool2 = maxpool2d(conv2)
+#last layer - fully connected layer?
+r_layer2 = tf.reshape(pool2, [-1, CONV_WINDOW_SIZE * CONV_WINDOW_SIZE * NUM_OF_CHANNELS_LAYER3])
+fc = tf.nn.relu(tf.matmul(r_layer2, weights['W_fc']) + biases['b_fc'])
+dropped_fc = tf.nn.dropout(fc, KEEP_RATE)
+
+score = tf.matmul(dropped_fc, weights['out']) + biases['out']
+actions_probs = tf.nn.softmax(score)
+
+#HERE STARTS THE GRADIENT COMPUTATION
+tvars = tf.trainable_variables()
+# rewards sums from k=t to T:
+rewards_arr = tf.placeholder(tf.float32, [1,None])
+# actions - a mask matrix which filters ys result accordint to the actions that were chosen.
+actions_mask = tf.placeholder(tf.bool, [None, OUTPUT_DIM])
+# return a T size vector with correct (chosen) action values
+filtered_actions = tf.boolean_mask(actions_probs, actions_mask)
+pi = tf.log(filtered_actions)
+#devide by T
+#loss = tf.divide(tf.reduce_sum(tf.multiply(pi,rewards_arr)),tf.to_float(tf.size(pi)))
+#don't devide by T
+loss = tf.reduce_sum(tf.multiply(pi,rewards_arr))
+Gradients = tf.gradients(-loss,tvars)
+
+#from here starts update weights
+Gradients_holder = [tf.placeholder(tf.float32) for i in range(VAR_NO)]
+# then train the network - for each of the parameters do the GD as described in the HW.
+#learning_rate = tf.placeholder(tf.float32, shape=[])   #TODO: maybe use later for oprimization of the model
+train_step = tf.train.AdamOptimizer(1e-2).apply_gradients(zip(Gradients_holder,tvars))
+
+
+#agent starts here:
+init = tf.global_variables_initializer()
+#init2 = tf.initialize_all_variables()
 def main():
-    a = np.array(range(60))
-    a = np.diff(np.cumsum(np.add((a%15==14).astype(np.int) , np.multiply((a%30==28).astype(np.int) , -1))))
-    print (a)
+    #variables used for models logics
+    raw_scores, states, actions_booleans = [BEGINING_SCORE], [], []
+    episode_number = 0
+    update_weights = False  # if to much time passed, update the weights even if the game is not finished
+    grads_sums = get_empty_grads_sums()  # initialize the gradients holder for the trainable variables
 
-    print(calc_reward_from_raw(a))
+    #variables for debugging:
+    manual_prob_use = 0
+    prob_deviation_sum = 0
+    default_data_counter = 0  # counts number of exceptions in reading the observations' file (and getting a default data)
+    step_counter = 0        #TODO: for tests
+    obsrv = []
+    #variables for evaluation:
 
-if __name__ == "__main__":
-    main()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        #sess.run(init2)     #TODO: check if this necessary
+
+        # check if file is not empty
+        if (os.path.isfile(WEIGHTS_FILE) and LOAD_WEIGHTS):
+            # Load with shmickle
+            with open(WEIGHTS_FILE, 'rb') as f:  # BEST_WEIGHTS
+                for var, val in zip(tvars, pkl.load(f)):
+                    sess.run(tf.assign(var, val))
+            print("loaded weights successfully!")
+
+        # creates file if it doesn't exisits:
+        if (not os.path.isfile(WEIGHTS_FILE)):
+            open(WEIGHTS_FILE, 'a').close()
+        if (not os.path.isfile(BEST_WEIGHTS)):
+            open(BEST_WEIGHTS, 'a').close()
+            print("created weights files sucessfully!")
+
+
+        while step_counter < MAX_GAMES:
+            #get data and process score to reward
+            obsrv, score, is_dead, request_id, default = get_observation()  # get observation
+            default_data_counter += default
+            #is_dead = False         #TODO: for debug
+            raw_scores.append(score)
+
+            #TODO: simple reward function
+            reward = get_reward(raw_scores, is_dead)
+
+            #TODO: for debug
+            vars = sess.run(tvars)
+
+            # append the relevant observation to the following action, to states
+            states.append(obsrv)        #TODO: use np.concatinate?
+            # Run the policy network and get a distribution over actions
+            action_probs = sess.run(actions_probs, feed_dict={observations: [obsrv]})
+            # np.random.multinomial cause problems
+            try:
+                chosen_actions = np.random.multinomial(1, action_probs[0])
+            except:
+                chosen_actions = pick_random_action_manually(action_probs[0])
+                manual_prob_use += 1
+                prob_deviation_sum += np.abs(np.sum(action_probs) - 1)
+
+            # Saves the selected action for a later use
+            actions_booleans.append(chosen_actions)
+            # index of the selected action
+            action = np.argmax(actions_booleans[-1])
+            #TODO: for debuggig
+            print("action_probs: " + str(action_probs))
+            print("observation got: " + str(obsrv))
+            print("action chosen: " + str(action))
+            print("manual_prob_use: " + str(manual_prob_use))
+            print("prob_deviation_sum: " + str(prob_deviation_sum))
+            print("default_data_counter: " + str(default_data_counter))
+            print("step_counter: "+str(step_counter))
+            # step the environment and get new measurements
+            send_action(action,request_id)
+            # add reward to rewards for a later use in the training step
+            #rewards.append(reward)
+            step_counter += 1  #TODO: this is for tests
+
+            #TODO: temporary, change to something that make sense...
+            if (step_counter % STEPS_UNTIL_BACKPROP) == 0:
+                update_weights = True
+
+            #TODO: sleep here?
+            time.sleep(0.05)
+
+            if is_dead or update_weights:
+                #UPDATE MODEL:
+
+                #calculate rewards from raw scores:
+                processed_rewards = calc_reward_from_raw(raw_scores, is_dead)
+                #TODO:delte this when it works
+                ''' 
+                # create the rewards sums of the reversed rewards array
+                rewards_sums = np.cumsum(rewards[::-1])
+                # normalize prizes and reverse
+                rewards_sums = decrese_rewards(rewards_sums[::-1])
+                '''
+                modified_rewards_sums = np.reshape(processed_rewards, [1, len(processed_rewards)])
+                # modify actions_booleans to be an array of booleans
+                actions_booleans = np.array(actions_booleans)
+                actions_booleans = actions_booleans == 1
+
+                #TODO: showind process results for debugging:
+                fa_res = sess.run(filtered_actions, feed_dict={observations: states, actions_mask: actions_booleans,
+                                                       rewards_arr: modified_rewards_sums})
+                pi_res = sess.run(pi, feed_dict={observations: states, actions_mask: actions_booleans,
+                                                       rewards_arr: modified_rewards_sums})
+                loss_res = sess.run(loss, feed_dict={observations: states, actions_mask: actions_booleans,
+                                                       rewards_arr: modified_rewards_sums})
+
+
+                # gradients for current episode
+                grads = sess.run(Gradients, feed_dict={observations: states, actions_mask: actions_booleans,
+                                                       rewards_arr: modified_rewards_sums})
+                grads_sums += np.array(grads)
+
+                episode_number += 1
+                update_weights = False
+
+                # Do the training step
+                if (episode_number % BATCH_SIZE == 0):
+                    grad_dict = {Gradients_holder[i]: grads_sums[i] for i in range(VAR_NO)}
+                    #TODO choose learning rate?
+                    # take the train step
+                    sess.run(train_step, feed_dict=grad_dict)
+                    #nullify grads_sum
+                    grads_sums = get_empty_grads_sums()
+
+                    #TODO: we don't want to save every time we update, this is for test and will be moved
+                    # manual save
+                    with open(WEIGHTS_FILE, 'wb') as f:
+                        pkl.dump(sess.run(tvars), f, protocol=2)
+                    print('auto-saved weights successfully.')
+
+                # nullify relevant vars and updates episode number.
+                raw_scores, states, actions_booleans = [BEGINING_SCORE], [], []
+                manual_prob_use = 0
+
+                wait_for_game_to_start()
+
+
+
+main()
