5ed51cd3a0ff287a23fe511265bca7c53e6cca62
Tom Garburg <tomgarburg@gmail.com>
2017-08-27 12:35:52 +0300

    correction to reward function with no numpy

--- utils/reward_utils.py
+++ utils/reward_utils.py
@@ -40,34 +40,35 @@ def calc_reward_from_raw(score_arr , is_dead):
         else:
             rewards.append(LARGE_GAIN_REWARD)
 
-
+    print(rewards)
     if is_dead:
         rewards[-1] = PUNISHMENT_FOR_DEATH
 
     #compute discounted rewards
-    cumulative_discounted_rewards = np.zeros_like(len(rewards))
-    partial_sum = 0
+    cumulative_discounted_rewards = np.zeros    (len(rewards))
+    partial_sum = 0.0
     for i in reversed(range(0, len(rewards))):
         partial_sum = partial_sum * DISCOUNT_FACTOR + rewards[i]
-        discounted_rewards[i] = partial_sum
+        cumulative_discounted_rewards[i] = partial_sum
 
     #boost relevant rewards
     #boosted_processed_rewards = boost( (rewards > 0) ,(rewards < NO_REWARD_PENALTY)
     #
     #                                    , cumulative_discounted_rewards)
-
+    print(cumulative_discounted_rewards)
     #normalize
-    cumulative_discounted_rewards \= np.std(cumulative_discounted_rewards)
-    cumulative_discounted_rewards -= np.mean(normalized_rewards)
+    cumulative_discounted_rewards /= np.std(cumulative_discounted_rewards)
+    cumulative_discounted_rewards -= np.mean(cumulative_discounted_rewards)
 
     return cumulative_discounted_rewards
 
 def main():
     a = np.array(range(60))
     a = np.diff(np.cumsum(np.add((a%15==14).astype(np.int) , np.multiply((a%30==28).astype(np.int) , -1))))
+    a*= 3
     print (a)
 
-    print(calc_reward_from_raw(a))
+    print(calc_reward_from_raw(a, True))
 
 if __name__ == "__main__":
     main()
