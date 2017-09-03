598b963172148187e8aa6ac001769f1b2c26bc0c
Yair <y4000r@gmail.com>
2017-09-03 17:58:10 +0300

    added function to visualize observation

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -9,6 +9,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import os
 import time
+from scipy import misc
 
 def plot_graph(points,title,file_name):
     directory = './graphs/'
@@ -21,17 +22,7 @@ def plot_graph(points,title,file_name):
     plt.savefig(file_name)
     plt.clf()
 
-'''
-def main():
 
-    a = [77.592619047619053, 56.003, 37.111249999999998, 24.974250000000001, 33.333013513513514, 27.578368421052637,
-     23.183499999999999, 44.660999999999987, 21.352898024750122, 25.579499999999999, 19.521507062146892,
-     28.124642156862741, 38.173099604823186, 21.128499999999999, 21.236499999999999, 31.360249999999997,
-     21.351999999999997, 25.872000000000003, 33.888416666666672, 132.47800000000001, 151.59022234392111]
-
-    plot_graph( a , "expert prefoemance over epochs" , "expert prefoemance over epochs")
-
-if __name__ == "__main__":
-    main()
-
-'''
+def obsrv_to_image(observ,files_name):
+    observ = misc.imresize(observ,2500);    #X25 bigger
+    misc.toimage(observ).save(files_name + '.png')
