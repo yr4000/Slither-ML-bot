9d05099d1d1663ff125d2994761cfd52da570f13
Yair <y4000r@gmail.com>
2017-09-10 20:51:45 +0300

    code refactoring

--- server.py
+++ server.py
@@ -2,29 +2,19 @@ from flask import Flask, render_template, request, redirect, Response, jsonify
 from flask_cors import CORS, cross_origin
 import json
 from datetime import datetime
-import time as t
 
 app = Flask(__name__)
 cors = CORS(app)#This is needed for the server to be able to send responses
-
-'''
-@app.route('/')
-def output():
-    return render_template('index.html', name='Joe')
-    #return redirect("This is a temporary implementation.")
-'''
 
 #this route will get the data from the client, send it to the agent and then send the result back to the client.
 @app.route('/model', methods = ['POST'])
 def ask_model():
     data = request.get_json(force=True)
     print("Request "+str(data['message_id'])+' sent in: '+str(data['hours'])+':'+str(data['minutes'])+':'+str(data['seconds']))
+
+    #saves observation to file
     with open('observation.json', 'w') as outfile:
         json.dump(data, outfile)
-
-    # TODO: add sleep? not a good idea since there si already a lag
-    #t.sleep(0.05)
-
 
     #Gets action from file
     try:
@@ -34,7 +24,7 @@ def ask_model():
         res = {'action': 0,
                'do_accelerate': 0,
                'request_id': -1,
-               'commit_sucide': False}        #TODO: change to a better default
+               'commit_sucide': False}
 
     #print(res)
     time = datetime.now().time()
